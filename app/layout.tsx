b9b8c1b2a39715d4fd2bc59c48c79cfdc2330d22
swapdicky <dw@swwwap.hk>
2026-05-26 21:11:00 +0100

    performance optimization for typekit fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://use.typekit.net" />
+        <link rel="dns-prefetch" href="https://use.typekit.net" />
+      </head>
       <body>
         {/* <Navigation /> */}
         <main>{children}</main>
